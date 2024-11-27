6d7bb5e189c82754a4727c651077475d0b7217e3
jhcao <1041630444@qq.com>
2024-11-27 17:08:06 +0800

    feat: add

--- components/header/yl-default-header.vue
+++ components/header/yl-default-header.vue
@@ -1,8 +1,20 @@
 <template>
   <view class="yl-default-header">
-    <view class="header_left" @click="leftClick">{{ leftTitle }}</view>
+    <view
+      :style="{ color: `${leftColor}` }"
+      class="header_left"
+      @click="leftClick"
+    >
+      {{ leftTitle }}
+    </view>
     <view class="header_title">{{ title }}</view>
-    <view class="header_right" @click="rightClick">{{ rightTitle }}</view>
+    <view
+      :style="{ color: `${rightColor}` }"
+      class="header_right"
+      @click="rightClick"
+    >
+      {{ rightTitle }}
+    </view>
   </view>
 </template>
 <script>
@@ -25,6 +37,16 @@ export default {
     rightTitle: {
       type: String,
       default: "确定"
+    },
+    // 左侧按钮颜色
+    leftColor: {
+      type: String,
+      default: "#999999"
+    },
+    // 右侧按钮颜色
+    rightColor: {
+      type: String,
+      default: "#14191E"
     },
   },
   data() {
