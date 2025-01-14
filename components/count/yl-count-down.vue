f5add4014d4fde69917bb9cab14d6165e5df8e40
cao1041630444 <cao1041630444@gmail.com>
2025-01-14 14:09:48 +0800

    feat: 问题修复

--- components/count/yl-count-down.vue
+++ components/count/yl-count-down.vue
@@ -1,30 +1,18 @@
 <template>
   <view class="yl-count-down">
-    <view v-for="(x, index) in countDays" :key="`${index + 'day'}`" class="box">
+    <view v-for="(x, index) in countDays" :key="index" class="box">
       {{ x }}
     </view>
     <view class="text">天</view>
-    <view
-      v-for="(x, index) in countHours"
-      :key="`${index + 'hour'}`"
-      class="box"
-    >
+    <view v-for="(x, index) in countHours" :key="index" class="box">
       {{ x }}
     </view>
     <view class="text">时</view>
-    <view
-      v-for="(x, index) in countMinutes"
-      :key="`${index + 'minutes'}`"
-      class="box"
-    >
+    <view v-for="(x, index) in countMinutes" :key="index" class="box">
       {{ x }}
     </view>
     <view class="text">分</view>
-    <view
-      v-for="(x, index) in countSeconds"
-      :key="`${index + 'second'}`"
-      class="box"
-    >
+    <view v-for="(x, index) in countSeconds" :key="indexs" class="box">
       {{ x }}
     </view>
     <view class="text">秒</view>
